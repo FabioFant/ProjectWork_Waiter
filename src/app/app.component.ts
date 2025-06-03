ce80c3443e50a8c66c288d052a227ba22bbb95a8
Fabio Fantini <fabiofanta07@gmail.com>
2025-06-03 16:43:44 +0200

    added login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
+import { AuthService } from './services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ProjectWork_Waiter';
+
+  constructor(public authService: AuthService)
+  {
+
+  }
 }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common"
+import { Component } from "@angular/core"
+import { FormsModule } from "@angular/forms"
+import { Router } from "@angular/router"
+import { AuthService } from "../../services/auth.service"
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username = ""
+  password = ""
+  errorMessage = ""
 
+  constructor(private authService: AuthService, private router: Router) 
+  {
+    if( authService.isLogged() ) router.navigate(['/'])
+  }
+
+  login()
+  {
+    this.errorMessage = ""
+    this.authService.login(this.username, this.password).subscribe({
+      next: r => this.router.navigate(['/']),
+      error: e => this.errorMessage = 'Credenziali non valide. Riprova.'
+    })
+  }
 }
