fbcdd8d55238f9d7fd6989d2685e799e4f775299
TheBarbox <andrea.barbozzi@gmail.com>
2025-06-03 16:45:25 +0200

    OrderComponent- done

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { WaiterService } from '../../services/waiter.service';
+import Order from '../../models/Order';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-order',
-  imports: [],
+  imports: [RouterModule, CommonModule],
   templateUrl: './order.component.html',
   styleUrl: './order.component.css'
 })
 export class OrderComponent {
+  tableId:number;
+  ordini:Order[] = [];
+
+  constructor(private route:ActivatedRoute, private service:WaiterService){
+    this.tableId = this.route.snapshot.params["id"]
+  }
+  GetTableOrder(){
+    /*this.service.GetTableOrder(this.tableId).subscribe({
+      next:r => this.ordini = r,
+      error:
+    })*/
+  }
 
 }
