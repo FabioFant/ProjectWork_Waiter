290e538775d023639ee904af9426957f707c90d6
kevin <kevinbardhi58@gmail.com>
2025-06-03 15:21:07 +0200

    refactor: remove unused tavolo component and add table-detail component with styles

---
+++ src/app/components/table-detail/table-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-detail',
+  imports: [],
+  templateUrl: './table-detail.component.html',
+  styleUrl: './table-detail.component.css'
+})
+export class TableDetailComponent {
+
+}
