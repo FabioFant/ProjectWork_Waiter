eaccaf4bb625dcbc24f361402e49b0db69cce558
Moli2112 <mattia.molinari.stud@ispascalcomandini.it>
2025-06-03 15:39:42 +0200

    first header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor (private router:Router) {}
+
+  Logout()
+  {
+    //this.authService.logout().subscribe( r => this.router.navigate(['login']));
+  }
 }
