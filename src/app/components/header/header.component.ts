827d52d428dcde28163966ba3d5caf9cf0c2c733
Moli2112 <mattia.molinari.stud@ispascalcomandini.it>
2025-06-04 10:18:48 +0200

    icona + billComponent

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -11,14 +11,34 @@ import { AuthService } from '../../services/auth.service';
 })
 export class HeaderComponent {
 
-  url:string;
   messaggio:string;
   id?:number;
   constructor (private route:ActivatedRoute, private router:Router, private authService:AuthService)
   {
-    this.url="";
-    console.log(router.url);
-    this.messaggio="";
+    this.messaggio=" ";
+    let urlSeparato=window.location.href.split('/');
+    let indice=-1;
+    for(let i=0;i<urlSeparato.length;i++)
+    {
+      if(urlSeparato[i]=='tables')
+      {
+        indice=i;
+        break;
+      }
+    }
+    if(indice==-1)
+    {
+      this.messaggio="Tables";
+    }
+    else
+    {
+      this.messaggio="Table "+urlSeparato[indice+1];
+      if((indice+1)<urlSeparato.length-1)
+      {
+        this.messaggio+=" - "+urlSeparato[indice+2];
+      }
+    }
+    console.log(window.location.href);
   }
 
   Logout()
