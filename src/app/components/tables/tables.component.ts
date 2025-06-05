8b557cb99cc049969e51c44023a27764f751637a
Fabio Fantini <fabiofanta07@gmail.com>
2025-06-05 15:45:26 +0200

    added auto data update

--- src/app/components/tables/tables.component.ts
+++ src/app/components/tables/tables.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { WaiterService } from '../../services/waiter.service';
 import Table from '../../models/Table';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { interval, Subscription, switchMap } from 'rxjs';
+import { enviroment } from '../../../enviroments/enviroment';
 
 @Component({
   selector: 'app-tables',
@@ -10,24 +12,46 @@ import { RouterLink } from '@angular/router';
   templateUrl: './tables.component.html',
   styleUrl: './tables.component.css'
 })
-export class TablesComponent {
+export class TablesComponent implements OnInit, OnDestroy {
+  private polling!: Subscription;
+
   tables: Table[] = [];
   available = '?';
   occupied = '?';
 
   constructor(private waiterService: WaiterService)
   {
-    this.waiterService.GetAllTables().subscribe(r => {
-      this.tables = r;
-      let count = 0;
-      this.tables.forEach(t => {
-        if (t.occupied) {
-          count++;
-        }
-      })
-
-      this.occupied = count.toString();
-      this.available = (this.tables.length - count).toString();
+    this.waiterService.GetAllTables().subscribe({
+      next: r => this.updateData(r),
+      error: err => console.error('Fetch error:', err)
     });
+  }
+
+  updateData(data: Table[]) 
+  {
+    this.tables = data;
+    const count = this.tables.filter(t => t.occupied).length;
+    this.occupied = count.toString();
+    this.available = (this.tables.length - count).toString();
+  }
+
+  ngOnInit() 
+  {
+    this.polling = interval(enviroment.pollingInterval)
+      .pipe(
+        // New request while ignoring the previous one if it hasn't completed yet
+        switchMap(() => this.waiterService.GetAllTables())
+      )
+      .subscribe({
+        next: r => this.updateData(r),
+        error: err => console.error('Polling error:', err)
+      });
+  }
+
+  ngOnDestroy() 
+  {
+    if( this.polling ) {
+      this.polling.unsubscribe();
+    }
   }
 }
