851680503e603bc8d8f5a024a0433e53db8af6d5
kevin <kevinbardhi58@gmail.com>
2025-06-06 16:47:01 +0200

    bug fix add order

--- src/app/components/add-order/add-order.component.ts
+++ src/app/components/add-order/add-order.component.ts
@@ -86,12 +86,14 @@ export class AddOrderComponent {
   }
 
   inviaOrdine(){
+    this.loading = true;
     const products = this.productsForCategory.flat().filter(product => product.qty > 0);
     if(products.length > 0) {
       this.service.AddTableOrder(this.tableId, products).subscribe({
         next: () => {
           // Clear local storage for products after order is sent
           products.forEach(product => localStorage.removeItem(product.name));
+          this.loading = false;
           this.router.navigate(['tables/' + this.tableId + '/order']);
         }
       });
